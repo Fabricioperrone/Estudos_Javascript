5f6e30602d4a69baffb95eaaf717bbccb35e22c0
Fabricio Acer <fabricio_luis@outlook.com>
2023-03-27 20:35:25 -0300

    Desafios JS

---
+++ exercicios-js/fundamentos/inicio/desafio_area.js
@@ -0,0 +1,11 @@
+// Desafio de PI
+
+const PI = 3.141532;
+const raio = 10;
+let areaCircunferencia = PI * raio * raio;
+
+console.log("O valor da área é: " + areaCircunferencia + "m2.");
+
+// usando o Math.PI do Js
+areaCircunferencia = Math.PI *  raio * raio;
+console.log("O valor da área é: " + areaCircunferencia + " m2.");
---
+++ exercicios-js/fundamentos/inicio/desafio_troca.js
@@ -0,0 +1,10 @@
+// Desafio troca de valores
+let a = 7;
+let b = 94;
+// essa variável armazena o valor de "a" antes dele mudar!
+let auxiliar = a;
+a = b;
+b = auxiliar;
+console.log("Exemplo 1")
+console.log(" A vale agora: " + a);
+console.log(" B agora vale: " + b);

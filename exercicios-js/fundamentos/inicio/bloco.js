faadcd8759bceb48eb78ea844148930dc2afef0f
Fabricio Acer <fabricio_luis@outlook.com>
2023-03-27 16:11:49 -0300

    Desafio_sentença_bloco

---
+++ exercicios-js/fundamentos/inicio/bloco.js
@@ -0,0 +1,15 @@
+{
+    console.log('Passo #01'); // exemplo de um bloco de código
+}
+
+{
+    console.log('Passo #01');
+    console.log('Passo #02');
+    console.log('Passo #03');
+}
+
+{
+    console.log('Passo #04');
+    console.log('Passo #05');
+}
+console.log("Fim!")
---
+++ exercicios-js/fundamentos/inicio/comentarios.js
@@ -0,0 +1,11 @@
+// consolle.log pe usado para exibir algo na tela
+console.log('Passo #03');
+console.log('Passo #02');// mais um exemplo de console.log
+//console.log('Passo #01');
+
+/*
+Esse é um comentário
+de multiplas
+linhas!
+*/
+console.log('Passo #01');

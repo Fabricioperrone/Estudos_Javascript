085a7fb845b18e790e548c18265ea9b7a46d4ccc
Fabricio Acer <fabricio_luis@outlook.com>
2023-03-27 19:26:45 -0300

    Constantes

--- exercicios-js/fundamentos/inicio/dados_4.js
+++ exercicios-js/fundamentos/inicio/dados_4.js
@@ -1,15 +1,9 @@
-/**
- * 3 tipos básicos que o Javascript aceita
- * 3 tipos básicos que o Javascript aceita
- */
-let idade = 31;
-console.log(typeof 31); // typeof exibe o tipo de dado
-console.log(typeof idade);
+const a = 3;
 
-let salario = 4578.32;
-console.log(typeof salario);
+// ..
 
-let estaChovendo = true; // ou false
-console.log(typeof estaChovendo);
+// a + a = 10;
 
-console.log(typeof "Exemplo de String");
+// ...
+
+console.log(a);

96ccff74d8428bccfb1bdd4a8769a843c7e3bd42
Fabricio Acer <fabricio_luis@outlook.com>
2023-03-27 18:26:57 -0300

    Dados_2

---
+++ exercicios-js/fundamentos/inicio/dados_2.js
@@ -0,0 +1,14 @@
+console.log(19.90 * 0.6);
+
+let preco = 19.90;
+let desconto = 0.4;
+let precoComDesconto = preco * (1 - desconto);
+console.log(precoComDesconto);
+
+
+let nome = "Caderno";
+let categoria = "Papepelaria";
+console.log("Produto: " + nome 
+ + ", Categoria: " + categoria
+ + ", Preço: " + preco
+ + ", Desconto: " + desconto);

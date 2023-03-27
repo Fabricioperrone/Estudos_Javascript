14c4b2da231e3f1d1d06d69f55ba9d24279c9cbe
Fabricio Acer <fabricio_luis@outlook.com>
2023-03-27 17:39:34 -0300

    Dados_1

---
+++ exercicios-js/fundamentos/inicio/dados_1.js
@@ -0,0 +1,11 @@
+let nomeProduto = "Caneta";
+let quantidade = 10;
+let preco = 6.4;
+let imposto = 1.5;
+let precoFinal = preco + imposto;
+
+console.log(nomeProduto);
+console.log(quantidade);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);

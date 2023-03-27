680a165bfdcebd9a380e3f3b5c3c66b07b4e7338
Fabricio Acer <fabricio_luis@outlook.com>
2023-03-27 19:12:34 -0300

    3 Tipos de dados Js

---
+++ exercicios-js/fundamentos/inicio/dados_3.js
@@ -0,0 +1,14 @@
+/**
+ * 3 tipos básicos que o Javascript aceita
+ */
+let idade = 31;
+console.log(typeof 31); // typeof exibe o tipo de dado
+console.log(typeof idade);
+
+let salario = 4578.32;
+console.log(typeof salario);
+
+let estaChovendo = true; // ou false
+console.log(typeof estaChovendo);
+
+console.log(typeof "Exemplo de String");

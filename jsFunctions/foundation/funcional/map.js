05cc8fa997ba21872efef2c3e787bda14ece1aee
Fabricio Acer <fabricio_luis@outlook.com>
2023-04-05 14:48:20 -0300

    Função map

---
+++ jsFunctions/foundation/funcional/map.js
@@ -0,0 +1,29 @@
+
+//usando a função MAP para tranformar um array em outro array
+
+const { elementAt } = require("rxjs")
+
+const numbers = [1, 2, 3, 4, 5, 6]
+
+const numbersV2 = numbers.map(elemento => elemento * 2)
+
+
+
+console.log(numbersV2)
+
+const students = [
+    { name: 'Jake', score: 6.4 },
+    { name: 'Susan', score: 8.6 },
+    { name: 'Emma', score: 9.4 },
+    { name: 'Peter', score: 9.1 },
+]
+
+const getScore = elemento => elemento.score
+
+const result = students
+
+    .map(getScore)
+    .map(Math.ceil) // arrendonda os valores para cima
+    .map(Math.floor) // arredonda para baixo
+
+console.log(students, result)

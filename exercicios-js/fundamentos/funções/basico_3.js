af74a13f44f9353c07ef0de56d81820a7052ab30
Fabrício Luis Perrone <suportesubzeroinformatica@gmail.com>
2023-06-26 13:15:05 -0300

    callback

--- exercicios-js/fundamentos/funções/basico_3.js
+++ exercicios-js/fundamentos/funções/basico_3.js
@@ -13,21 +13,42 @@ console.log(saudacao('Fabrício'))
 const somar = (...numeros) => {
     console.log(Array.isArray(numeros))
     let total = 0
-    for(let n of numeros) {
+    for (let n of numeros) {
         total += n
     }
     return total
 }
 
 
-console.log(somar(1,2,3))
-console.log(somar(1,2,3,4,5,6))
-console.log(somar(1,2,3,4,5,6,7,8,9,10))
+console.log(somar(1, 2, 3))
+console.log(somar(1, 2, 3, 4, 5, 6))
+console.log(somar(1, 2, 3, 4, 5, 6, 7, 8, 9, 10))
 
-const potencia = (base) => {
-    return (exp) => {
-        return Math.pow(base, exp)
-    }
+const potencia = base => exp =>  Math.pow(base, exp)
+console.log(potencia(2)(10))
+
+// this
+Array.prototype.log = function() {
+    console.log(this)
 }
 
-console.log(potencia(2)(8))
+const numeros = [1,2,3]
+numeros.log()
+
+// ultimo elemento
+
+Array.prototype.ultimo = function() {
+    console.log(this[this.length - 1])
+}
+
+const numeros2 = [1,2,3]
+numeros.ultimo()
+
+// primeiro
+
+Array.prototype.primeiro = function() {
+    console.log(this[this.length - 1])
+}
+
+const numeros3 = [1,2,3]
+numeros.primeiro()
---
+++ exercicios-js/fundamentos/funções/callback_1.js
@@ -0,0 +1,17 @@
+function exec(fn, a, b) {
+    return fn(a,b)
+}
+
+const somarNoTerminal = (x, y) => console.log(x + y)
+const subtrairNoTerminal = (w, z) => console.log(w - z)
+const subtrair = (w, z) => (w - z)
+
+exec(somarNoTerminal, 56, 38)
+exec(subtrairNoTerminal, 182, 27)
+
+const r = exec(subtrair, 50, 25)
+console.log(r)
+
+
+const cb = () => console.log('Exec...')
+setInterval(cb, 1000)
